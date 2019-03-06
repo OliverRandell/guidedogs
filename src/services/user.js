56846d32a1a43b6ea421890f95af227fcdf0b53d
Jay <holla@jaylinsell.com>
2019-03-06 16:05:14 +1100

    Updated login requests to retrieve token

--- src/services/user.js
+++ src/services/user.js
@@ -15,19 +15,24 @@ function login(username, password) {
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({ Username: username, Password: password, Email: username })
     };
-    return fetch(`${apiUrl}/users/authenticate`, requestOptions)
-        .then(handleResponse)
-        .then(user => {
-            // login successful if there's a jwt token in the response
-            if (user.token) {
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem('user', JSON.stringify(user));
-            }
+    return fetch(`${apiUrl}/authentication`, requestOptions)
+        .then(res => {
+            const reader =  res.body.getReader()
+            return reader.read().then(r => JSON.parse(Utf8ArrayToStr(r.value)))
+        })
+        .then(response => console.log('response', response.token))
+        // .then(handleResponse)
+        // .then(user => {
+        //     // login successful if there's a jwt token in the response
+        //     if (user.token) {
+        //         // store user details and jwt token in local storage to keep user logged in between page refreshes
+        //         localStorage.setItem('user', JSON.stringify(user));
+        //     }
 
-            return user;
-        });
+        //     return user;
+        // });
 }
 
 function logout() {
@@ -100,4 +105,46 @@ function handleResponse(response) {
 
         return data;
     });
+}
+
+// Decodes Uint8Array from initial response
+const Utf8ArrayToStr = array => {
+    var out, i, len, c;
+    var char2, char3;
+
+    out = "";
+    len = array.length;
+    i = 0;
+    while (i < len) {
+        c = array[i++];
+        switch (c >> 4) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+                // 0xxxxxxx
+                out += String.fromCharCode(c);
+                break;
+            case 12:
+            case 13:
+                // 110x xxxx   10xx xxxx
+                char2 = array[i++];
+                out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+                break;
+            case 14:
+                // 1110 xxxx  10xx xxxx  10xx xxxx
+                char2 = array[i++];
+                char3 = array[i++];
+                out += String.fromCharCode(((c & 0x0F) << 12) |
+                    ((char2 & 0x3F) << 6) |
+                    ((char3 & 0x3F) << 0));
+                break;
+        }
+    }
+
+    return out;
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-export const apiUrl = 'http://localhost:8080'
+export const apiUrl = 'https://gdvpeersupportplatformapi.azurewebsites.net/api'
 
 const mocks = {
     'auth': { 'POST': { token: 'This-is-a-mocked-token' } },
