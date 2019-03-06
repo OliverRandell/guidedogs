9e7516efa4f8b2ea0f68b60c343161147adf3e01
Oliver Randell <oliverrandell@gmail.com>
2019-03-06 16:32:09 +1100

    Adding further form validation to the login and register frontend.

--- src/components/common/auth/form-login.vue
+++ src/components/common/auth/form-login.vue
@@ -2,13 +2,14 @@
     <form @submit.prevent="handleSubmit">
         <div class="form-group">
             <label for="username">Username</label>
-            <input type="text" v-model="username" name="username" class="form-control" :class="{ 'is-invalid': submitted && !username }" />
+            <input type="text" v-model="username" name="username" class="form-control" :class="{ 'is-invalid': submitted && !username }" v-validate="'required'" />
             <div v-show="submitted && !username" class="invalid-feedback">Username is required</div>
         </div>
         <div class="form-group">
             <label htmlFor="password">Password</label>
-            <input type="password" v-model="password" name="password" class="form-control" :class="{ 'is-invalid': submitted && !password }" />
-            <div v-show="submitted && !password" class="invalid-feedback">Password is required</div>
+            <input type="password" v-model="password" name="password" class="form-control" :class="{ 'is-invalid': submitted && !password }" v-validate="'required|confirmed:password'" />
+            <!-- <div v-show="submitted && !password" class="invalid-feedback">Password is required</div> -->
+            <div v-if="submitted && errors.has('password')" class="invalid-feedback">{{ errors.first('password') }}</div>
         </div>
 
         <div class="form-group">
