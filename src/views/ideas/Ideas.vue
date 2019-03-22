48c453ddefd7e834a52dde484dc63a78afe2c015
anewport <anewport@dxc.com>
2019-03-22 12:03:28 +1100

    add ideas store

--- src/views/ideas/Ideas.vue
+++ src/views/ideas/Ideas.vue
@@ -12,32 +12,22 @@
         <div class="container">
             <div class="pg-content">
                 <section class="item-wrapper">
-                    <article class="idea-pod" v-for="idea in eventIdeas" :key="idea.id">
-                        <router-link :to="'/ideas/' + idea.id">
+                    <article class="idea-pod" v-for="idea in allIdeas" :key="idea.eventId">
+                        <router-link :to="'/ideas/' + idea.eventId">
                             <h2 class="title">{{ idea.title }}</h2>
                         </router-link>
 
-                        <p class="host"><span>Host:</span> {{ idea.hostName }}</p>
-                        <p>{{ idea.text }}</p>
-                        <router-link :to="'/ideas/' + idea.id" class="btn btn-primary">Read more</router-link>
-                        <!-- <button type="button" :class="{ interested: idea.interested === 'true' }" @click="idea.interested = !idea.interested">
-                            <span>Interested</span>
-                        </button> -->
+                        <p class="host"><span>Host:</span> {{ idea.eventHostNickname }}</p>
+                        <p>{{ idea.eventDetails | truncate }}</p>
+                        <router-link :to="'/ideas/' + idea.eventId" class="btn btn-primary">Read more</router-link>
                     </article>
                 </section>
                 <aside class="filters" role="group">
-                    <h4>Filter Ideas</h4>
-                    <button type="button" :class="['btn-filter', { active: currentFilter === 'all' }]" @click="setFilter('all')">All</button>
-                    <button type="button" :class="['btn-filter', { active: currentFilter === 'information' }]" @click="setFilter('information')">Information</button>
-                    <button type="button" :class="['btn-filter', { active: currentFilter === 'social' }]" @click="setFilter('social')">Social</button>
-                    <button type="button" :class="['btn-filter', { active: currentFilter === 'sport' }]" @click="setFilter('sport')">Sport and fitness</button>
-                    <button type="button" :class="['btn-filter', { active: currentFilter === 'art' }]" @click="setFilter('art')">Arts</button>
-
+                    <EventListingFilter v-on:filter-events="filterEvents" v-bind:currentFilter="currentFilter" />
                 </aside>
 
                 <article class="ideas-footer">
                     <p>Can't find what you're looking for? Create your own idea!</p>
-                    <!-- <button type="button" >Create idea</button> -->
                     <router-link to="/create-idea" class="btn btn-primary">Create idea</router-link>
                 </article>
 
@@ -47,60 +37,62 @@
 </template>
 
 <script>
+    import { mapGetters, mapActions } from "vuex";
     import LayoutMaster from '../../components/common/layouts/layout-master.vue';
     import Hero from '../../components/common/global/hero.vue';
+    import EventListingFilter from '../events/EventListingFilter.vue';
     export default {
         name: 'Ideas',
+
         components: {
             LayoutMaster,
-            Hero
+            Hero,
+            EventListingFilter,
         },
+
+        filters: {
+            truncate: function(value, limit) {
+                if (value && value.length > 100) {
+                    value = value.substring(0, 90) + '...';
+                }
+                return value;
+            }
+        },
+
         data() {
             return {
                 title: 'Ideas!',
                 tagline: 'Expressions of interest posted here',
-                eventIdeas: [
-                    {
-                        id: 0,
-                        title: 'The Divas',
-                        hostId: '',
-                        hostName: 'Dorothy123',
-                        text: "Our motto is: 'Women getting on with life'",
-                        interested: true,
-
-                    },
-                    {
-                        id: 1,
-                        title: 'Coffee chats in Kew',
-                        hostId: '',
-                        hostName: 'karen_007',
-                        text: `I'm looking to socialise with like-minded coffee loving individuals`,
-                        interested: false,
-                    }
-                ]
+                currentFilter: 'all',
             }
         },
-        created() {
-            this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data) {
-                this.ideas = data.body.slice(0,4);
-            })
+
+        computed: {
+            ...mapGetters(['allIdeas']),
         },
-        // computed: {
-        //     eventClasses: function() {
-        //         return {
-        //             interested: this.interested,
-        //         }
-        //     }
-        // }
+
+        created() {
+            this.getIdeas();
+        },
+
+        methods: {
+            ...mapActions({
+                'getIdeas': 'getIdeas',
+            }),
+            filterEvents: function(filter) {
+                this.currentFilter = filter;
+                this.getIdeas(filter);
+            },
+        }
     }
 </script>
 
 <style lang="scss" scoped>
     @import './src/assets/scss/vue.scss';
     .idea-pod {
-        @include spacer(1rem);
+        @include spacer(1.5rem);
         .title {
-            font-size: $font-size-base;
+            @include font-size($h2-font-size);
         }
         .host {
             font-size: $font-size-base*0.8;
